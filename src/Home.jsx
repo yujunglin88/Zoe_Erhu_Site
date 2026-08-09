d37021318d7a95d5d358ab79d865063918cb1954
Jeff <yujunglin88@gmail.com>
2026-08-10 10:31:48 +1200

    css fixes and start fixing layout issues

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,9 @@ import Navbar from './componenets/Navbar';
 function Home() {
   return (
     <>
+      <div>
       <Navbar />
-
+      </div>
       <div className="Home">
       <header className="Home-header">
         <img src={logo} className="Home-logo" alt="logo" />
