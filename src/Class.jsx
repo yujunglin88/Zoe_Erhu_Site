ef88d27294a623a2cf48fb8a82b266986d2fb03c
Jeff <yujunglin88@gmail.com>
2026-08-10 13:29:10 +1200

    css fixing navbar overlapping

--- src/Class.jsx
+++ src/Class.jsx
@@ -1,5 +1,3 @@
-import logo from './res/logo.png';
-import portrait from './res/portrait.jpg';
 import './Class.css';
 import NavBar from './componenets/Navbar';
 
@@ -9,8 +7,6 @@ function Class() {
       <NavBar/>
       <div className="Class">
         <header className="Class-header">
-          <img src={logo} className="Class-logo" alt="logo" />
-          <img src={portrait} className="Class-portrait" alt="portrait" />
           <h1 className="Class-heading">Classes</h1>
         </header>
       </div>
--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import icon from '../res/icon.png';
 import './Navbar.css';
 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <Link to="/" className="navbar-title">
+        <img src={icon} alt="Logo" className="navbar-logo-image" />
+        <span className="navbar-logo-text">Zoe Li Erhu Studio</span>
+      </Link>
+
       <Link to="/" className="navbar-link">
         Home
       </Link>
