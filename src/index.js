402c391264f43da77053672758251255445cb60f
Jeff <yujunglin88@gmail.com>
2026-08-11 10:13:15 +1200

    moved index.css into global

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/global.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
